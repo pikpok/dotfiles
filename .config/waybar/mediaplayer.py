f93ade8971349b20169c75126049ece3b918b8ef
Krzysztof Bezrąk <pikpok@riseup.net>
2021-04-07 22:54:31 +0200

    waybar: update mediaplayer script

--- .config/waybar/mediaplayer.py
+++ .config/waybar/mediaplayer.py
@@ -39,6 +39,8 @@ def on_metadata(player, metadata, manager):
     elif player.get_artist() != '' and player.get_title() != '':
         track_info = '{artist} - {title}'.format(artist=player.get_artist(),
                                                  title=player.get_title())
+    else:
+        track_info = player.get_title()
 
     if player.props.status != 'Playing' and track_info:
         track_info = ' ' + track_info
@@ -78,7 +80,7 @@ def signal_handler(sig, frame):
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
-    # Increase verbosity with every occurance of -v
+    # Increase verbosity with every occurence of -v
     parser.add_argument('-v', '--verbose', action='count', default=0)
 
     # Define for which player we're listening
